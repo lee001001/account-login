3e367891dac76748192a9da49493a8b0008b1ed7
King LEE <a0988698776@gmail.com>
2020-09-25 15:35:56 +0800

    fetch: create text account

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require('express')
-const methodOverride = require('method-override')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const PORT = 3000
 
-app.use(bodyParser.urlencoded({ extender: true }))
-app.use(methodOverride('_method'))
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+// 設定首頁Post
+app.post('/', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  console.log('The Account:', email)
+  console.log('The password:', password)
+
+  if (email === 'a1234@gmail') {
+    // console.log('verifiedAccount:', verifiedAccount);
+    res.render('welcome')
+  }
 })
 
 // 設定 port 3000
